50266a5fe0f61af993325b850af7662014eefdaf
David Smith <dbsmith.dbs83@gmail.com>
2016-07-12 22:01:04 -0700

    width issues fixed

--- js/app.js
+++ js/app.js
@@ -2,12 +2,6 @@
 // David Smith
 /////////////////////////////////////////////
 
-function loadContent() {  //Load entries into web page via jQuery
-  // for (var e = 0; e < entries.length; e++) {
-  //   entries[e].toHTML();
-  // }
-}
-
 function main() {
   if (window.innerWidth <= 680) { //if on mobile device..
     $('#main').accordion();
@@ -18,9 +12,10 @@ window.onresize = function() {
   if (window.innerWidth <= 680) {
     main();
   } else {
-    $('#main').accordion('destroy');
+    if ($('#main').hasClass('ui-accordion')) {
+      $('#main').accordion('destroy');
+    }
   }
 };
 
 $(document).ready(main);
-loadContent();
