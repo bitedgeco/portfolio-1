0174c5400bac987624ebbdeadedd3b78f4521c27
David Smith <dbsmith.dbs83@gmail.com>
2016-07-14 11:13:37 -0700

    click listeners on nav images, converted images to objects

--- js/app.js
+++ js/app.js
@@ -5,12 +5,11 @@ var numImages = 0;
 
 function generateContent() {
   //adds template, removes current content and updates with information in entries, removes template, sets nav img sizes
-  htmlEntries = [];
+  var htmlEntries = [];  //tmp array constructed to refresh content upon entries changes and append to page
   numImages = 0;
 
   $('#main').html('<entry class="template"><h3 class="sub-headings"><a class="anchor" name="namehere"></a>section</h3><div><time pubdate datetime="2000-01-01">Publish Time</time><p>content here</p></div></entry>');
   $('.nav-menu').html('');
-
 
   entries.forEach(function(e) {
     htmlEntries.push(new Entry(e));
@@ -19,11 +18,11 @@ function generateContent() {
   htmlEntries.sort(function(a,b) {
     return (new Date(b.date)) - (new Date(a.date));
   });
-
+//todo: make img an object with name property and onclick event!
   htmlEntries.forEach(function(e) {
     $('#main').append(e.toHTML());
     if (e.navImg) {
-      $('.nav-menu').append('<li><a href=\"#' + e.name + '\"><img src=\"' + e.navImg + '\" class=\"nav-icon\"/></a></li>');
+      e.navImg.renderImg(e.navImg);
     } else {
       console.log('no image supplied');
     }
--- js/entries.js
+++ js/entries.js
@@ -1,5 +1,3 @@
-var htmlEntries = [];
-
 // Entry object
 function Entry(info) {
   this.name = info.name;
@@ -7,10 +5,41 @@ function Entry(info) {
   this.date = info.date;
   this.text = info.text;
   if (info.navImg) {
-    this.navImg = 'img/' + info.navImg;
+    this.navImg = info.navImg;
     numImages++;
   }
 }
+
+function Img(name, url) {
+  this.name = name;
+  this.url = url;
+  var t = this; //used to reference later inside a method
+
+  this.clickEvent = function(img) {
+    console.log(img + ' clicked');
+  };
+
+  this.createListener = function(imgID) { //find <img> with id matching e, add listener to it
+    var $images = $('.nav-menu img');
+    var $thisImg;
+    $images.each(function() {
+      if ($(this).attr('id') === imgID) {
+        $thisImg = $(this);
+      }
+    });
+    $thisImg.on('click', function() {
+      t.clickEvent(imgID);
+    });
+  };
+
+  this.renderImg = function(navImg) {
+    if (navImg) {
+      $('.nav-menu').append('<img src=\"img/' + navImg.url + '\" class=\"nav-icon\" id="' + navImg.name + '" /></a></li>');
+      navImg.createListener(navImg.name);
+    }
+  };
+}
+
 
 Entry.prototype.toHTML = function() {
   var $newEntry = $('entry.template').clone();
@@ -20,7 +49,7 @@ Entry.prototype.toHTML = function() {
   $newEntry.find('p').text(this.text);
   $newEntry.find('time').attr('pubdate', this.date);
   $newEntry.find('time[pubdate]').attr('title', this.date);
-  $newEntry.find('time').html('about ' + parseInt(Math.round((new Date() - new Date(this.date))/60/60/24/1000)) + ' days ago');
+  $newEntry.find('time').html('-posted about ' + parseInt(Math.round((new Date() - new Date(this.date))/60/60/24/1000)) + ' days ago');
 
   return $newEntry.children();  //putting children() here removes entry tag for accordion menu to work
 };
@@ -31,28 +60,28 @@ var entries = [
     name: 'about',
     section: 'About Me',
     date: '2016-07-12',
-    navImg: 'person.jpg',
+    navImg: new Img('about', 'person.jpg'),
     text: 'Hello, I am David, and this is my portfolio site.  Hopefully after browsing this site, you can get a sense of who I am and where I am going.  I began making this portfolio as a student at Code Fellows in July 2016.  At Code Fellows, I am studying to become a competitive developer, and plan on working as a data analyst.  I enjoy working with data in general, and find solving problems with it fun and rewarding.'
   },
   {
     name: 'edu',
     section: 'Education',
     date: '2016-07-12',
-    navImg: 'edu.png',
+    navImg: new Img('edu', 'edu.png'),
     text: 'I graduated in 2015 with a B.S. in chemistry and a B.A. in biochemistry from the University of Washington.  My favorite subjects were chemical biology, quantitative analysis, and statistical mechanics.  I joined Code Fellows in the summer of 2016, and am currently on track to learn Python.  If you click onmy GitHub link at the top of the page, you can see some of the projects that I have been working on.'
   },
   {
     name: 'exp',
     section: 'Experience',
     date: '2016-07-12',
-    navImg: 'work.jpg',
-    text: 'Throughout my experience as a working member of society, I have worked as a cashier, a QA technician at a tire factory, and a server at many restaurants.  I am in the process of taking what I have learned and changing my career to the technology field, incorporating my education and experience.'
+    navImg: new Img('exp', 'work.jpg'),
+    text: 'Throughout my experience as a working member of society, I have worked as a cashier, a QA technician at a tire factory, and a server at many restaurants.  I am in the process of taking what I have learned and changing my career to the technology field, incorporating my education and experiences.'
   },
   {
     name: 'int',
     section: 'Interests',
     date: '2016-07-12',
-    navImg: 'interests.png',
+    navImg: new Img('int', 'interests.png'),
     text: 'When I\'m not coding, I have several hobbies keeping me busy.  I enjoy taking vacations, with my most recent trip being to Iceland.  It was one of the most beautiful places I have ever seen.  I also enjoy brewing beer.  I am currently fermenting a ginger beer for the first time.  For video games, I have been playing Fallout 4 and dying a lot in Dark Souls III.  I am also in one of the most in-depth fantasy football leagues ever made.'
   }
 ];
