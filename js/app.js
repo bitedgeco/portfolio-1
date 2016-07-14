88db6d2a4b8002a63b0df740a053577f5351ad84
David Smith <dbsmith.dbs83@gmail.com>
2016-07-14 12:43:53 -0700

    fixed addNewEntry

--- js/app.js
+++ js/app.js
@@ -3,14 +3,23 @@
 /////////////////////////////////////////////
 var numImages = 0;
 
+function adjustNavImageSize() {
+  // resizes nav images for when more populate
+  var x = (450 / numImages).toString() + 'px';
+  $('.nav-menu img').width(x);
+  var m = (60 / numImages).toString() + 'px';
+  $('.nav-menu img').css('margin-left', m);
+}
+
 function generateContent(img) {
   //adds template, removes current content and updates with information in entries, removes template, sets nav img sizes
   var htmlEntries = [];  //array constructed to refresh content upon entries changes and append to page
   numImages = 0;
 
-  if (img) {
+  if (img) {  //if img parameter was given by navImg event handler
     for(var i = 0; i < entries.length; i++) {
       if (entries[i].name === img) {
+        $('#main').hide();
         $('#main').html('<entry class="template"><h3 class="sub-headings"><a class="anchor" name="namehere"></a>section</h3><div><time pubdate datetime="2000-01-01">Publish Time</time><p>content here</p></div></entry>');
         htmlEntries.push(new Entry(entries[i]));
         htmlEntries.sort(function(a,b) {
@@ -19,12 +28,12 @@ function generateContent(img) {
         htmlEntries.forEach(function(e) {
           $('#main').append(e.toHTML());
         });
+        $('#main').fadeIn();
       }
     };
     $('.template').remove();
     return;
   }
-
 
   $('#main').html('<entry class="template"><h3 class="sub-headings"><a class="anchor" name="namehere"></a>section</h3><div><time pubdate datetime="2000-01-01">Publish Time</time><p>content here</p></div></entry>');
   $('.nav-menu').html('');
@@ -47,11 +56,7 @@ function generateContent(img) {
   });
 
   $('.template').remove();
-  // resizes nav images for when more populate
-  var x = (450 / numImages).toString() + 'px';
-  $('.nav-menu img').width(x);
-  var m = (60 / numImages).toString() + 'px';
-  $('.nav-menu img').css('margin-left', m);
+  adjustNavImageSize();
 }
 
 function addNewEntry(name1, section1, text1, navImg1) {
@@ -63,6 +68,7 @@ function addNewEntry(name1, section1, text1, navImg1) {
 
 
 window.onresize = function() {
+  adjustNavImageSize();
   if (window.innerWidth <= 680) {
     $('#main').accordion();
   } else {
